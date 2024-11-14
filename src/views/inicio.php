3376052975a51d5acca449a4514692c279988e63
Pablo Alicante <pabloalicanteat@hotmail.com>
2024-11-14 10:28:31 +0100

    Barra de busqueda

--- src/views/inicio.php
+++ src/views/inicio.php
@@ -1,4 +1,11 @@
 <h1 id="tituloMovil">RoofScout.</h1>
+
+<form action="/busqueda" method="get" class="search-bar">
+      <input type="text" name="query" placeholder="Buscar...">
+      <button type="submit" class="fa fa-search"></button>
+</form>
+    
+
 <div id="galeria">
         <?php foreach ($anuncios as $anuncio): ?>
             <?php include('./src/views/templates/card.inc.php'); ?>
